453da6c2ddd8f3aa15cce3c5ea018c4767a89203
Léon <leon.bonnet@ecal.ch>
2025-04-24 11:33:42 +0200

    mise a jourss 24.04.2025
    midif css

--- src/firebase.js
+++ src/firebase.js
@@ -1,40 +1,29 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth} from 'firebase/auth';
-import { getFirestore, collection, getDocs} from 'firebase/firestore';
-import { getStorage} from 'firebase/storage';
-
-
-
-
-
-
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Votre configuration Firebase
 const firebaseConfig = {
-    projectId: "d4studio-84b9d",
-    apiKey: "AIzaSyAxRjt8OUhAMmikv9WeWCY9hW2-DPoCFgc",
-    storageBucket: "gs://d4studio-84b9d.firebasestorage.app",
+  projectId: "d4studio-84b9d",
+  apiKey: "AIzaSyAxRjt8OUhAMmikv9WeWCY9hW2-DPoCFgc",
+  storageBucket: "gs://d4studio-84b9d.firebasestorage.app",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 // Initialize Firestore
 const db = getFirestore(app);
 
 // Fonction pour récupérer des documents depuis Firestore
 async function getData(collectionName) {
-    let data = [];
-    const querySnapshot = await getDocs(collection(db, collectionName));
-    querySnapshot.forEach((doc) => {
-        data.push({ id: doc.id, ...doc.data() });
-    });
-    return data;
+  let data = [];
+  const querySnapshot = await getDocs(collection(db, collectionName));
+  querySnapshot.forEach((doc) => {
+    data.push({ id: doc.id, ...doc.data() });
+  });
+  return data;
 }
 
-
-
-
-
-export { getData};
+export { getData };
